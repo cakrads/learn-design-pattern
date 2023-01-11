f2ba75409532adad38d21c2342334c51aaef5b79
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-01-11 22:40:32 +0700

    feat: add class srp

---
+++ src/patterns/1-solid/1-srp/2-class.ts
@@ -0,0 +1,50 @@
+// Not Use SRP
+export class Auth {
+  email: string;
+  password: string;
+
+  constructor(email: string, password: string) {
+    this.email = email;
+    this.password = password;
+  }
+
+  async login() {
+    try {
+      const result = await fetch("API");
+      return result;
+    } catch (error: any) {
+      this.errorLog(error.message);
+    }
+  }
+
+  errorLog(message: string) {
+    console.log(message);
+  }
+}
+
+// use SRP
+// split responsibility logging and login
+export class ErrorLog {
+  static log(message: string) {
+    console.log(message);
+  }
+}
+
+export class AuthSRP {
+  email: string;
+  password: string;
+
+  constructor(email: string, password: string) {
+    this.email = email;
+    this.password = password;
+  }
+
+  async login() {
+    try {
+      const result = await fetch("API");
+      return result;
+    } catch (error: any) {
+      ErrorLog.log(error.message);
+    }
+  }
+}
